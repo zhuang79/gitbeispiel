4c6c35d96b44dc7b8b19f053285b2f99b5c29e65
Zhuang <x09-8qc-u1@fh-h.de>
2023-05-10 17:18:38 +0200

    Variable ob mit 10 für die obere Greanz

--- Main.java
+++ Main.java
@@ -11,11 +11,19 @@ public class Main
     public static void main(String args[])
     {
         int summe = 0;
+        int ob = 10;
         for(int i= 1 ; i <= 10 ; i++)
         {
             summe = summe +i;
         
         }
-    
+        
+        int mult = 1;
+        for(int i = 2 ; i <= ob ; ++i)
+        {
+            mult *=i;
+        
+        }
+        
     }
 }
