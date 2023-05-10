e352d388a34d0ca04a52a3556f6d4758c6f048ae
Zhuang <x09-8qc-u1@fh-h.de>
2023-05-10 16:54:14 +0200

    Summe wurde impelementiert

--- Main.java
+++ Main.java
@@ -8,4 +8,14 @@
 
 public class Main
 {
+    public static void main(String args[])
+    {
+        int summe = 0;
+        for(int i= 1 ; i <= 10 ; i++)
+        {
+            summe = summe +i;
+        
+        }
+    
+    }
 }
